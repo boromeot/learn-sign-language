95def2057defee567a0268d8fbdb076bde7e475b
boromeot <boromeotyler@gmail.com>
2023-08-26 10:44:23 -0700

    updated model, now it is much more accurate and percise

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useRef, useEffect } from 'react'
 import { useInterval } from './hooks';
 import Webcam from 'react-webcam'
-import * as handtrack from '@tensorflow-models/handpose';
+import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
+import '@tensorflow/tfjs-core';
+// Register WebGL backend.
 import '@tensorflow/tfjs-backend-webgl';
+import '@mediapipe/hands';
 import './App.css'
 
 function App() {
@@ -22,40 +25,47 @@ function App() {
     const ctx = canvasRef.current.getContext('2d');
 
     const detection = await model.estimateHands(video);
-    if (detection.length > 0) {
-      const { landmarks } = detection[0];
-      const { annotations } = detection[0];
-      for (const [x, y, z] of landmarks) {
-        ctx.beginPath();
-        ctx.arc(x, y, 2, 0, 2 * Math.PI);
-        ctx.fillStyle = 'violet';
-        ctx.fill();
-        ctx.closePath();
-      }
-
-      for (const key in annotations) {
-        const coords = annotations[key];
-        ctx.beginPath();
-        for (let i = coords.length - 1; i >= 0; i--) {
-          const [x, y, z] = coords[i];
-          ctx.lineTo(x, y);
+      for (let i = 0; i < detection.length; i++) {
+        const { keypoints } = detection[i];
+        for (const {x, y} of keypoints) {
+          ctx.beginPath();
+          ctx.arc(x, y, 2, 0, 2 * Math.PI);
+          ctx.fillStyle = 'violet';
+          ctx.fill();
+          ctx.closePath();
         }
-        ctx.lineTo(annotations['palmBase'][0][0], annotations['palmBase'][0][1])
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'green';
-        ctx.stroke();
+
+        ctx.beginPath();
+        for (let i = 0; i < keypoints.length; i++) {
+          const {x, y} = keypoints[i];
+          if ((i - 1) % 4 === 0) ctx.lineTo(keypoints[0].x, keypoints[0].y);
+          ctx.lineTo(x, y);
+          if ((i) % 4 === 0) {
+            ctx.stroke();
+            ctx.closePath();
+            ctx.beginPath();
+          }
+          
+        }
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = 'black';
         ctx.closePath();
       }
-
-    }
   }
 
   useEffect(() => {
     // Load mediapipe hand detection model
     const loadModel = async () => {
-      const model = await handtrack.load();
-      setModel(model);
-      console.log(model);
+      const model = handPoseDetection.SupportedModels.MediaPipeHands;
+      const detectorConfig = {
+        runtime: 'mediapipe',
+        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands',
+                      // or 'base/node_modules/@mediapipe/hands' in npm.
+        maxHands: 2,
+        modelType: 'full',
+      };
+      const detector = await handPoseDetection.createDetector(model, detectorConfig);
+      setModel(detector);
     }
 
     loadModel();
