85a0b011bd86dba95fb17362cb5fb3fa724fb16d
boromeot <boromeotyler@gmail.com>
2023-08-28 12:44:04 -0700

    fixed major bug where model tried detecting video before video was ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,68 @@
-import { useState, useRef, useEffect } from 'react'
-import { useInterval } from './hooks';
+import React, { useState, useRef, useEffect } from 'react';
 import { draw, setHandDetector } from './utills';
 import Webcam from 'react-webcam';
 import './App.css'
-
-import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
-import '@tensorflow/tfjs-core';
-// Register WebGL backend.
-import '@tensorflow/tfjs-backend-webgl';
-import '@mediapipe/hands';
 
 function App() {
   const webCamRef = useRef(null);
   const canvasRef = useRef(null);
   const [model, setModel] = useState(null);
 
-  const run = async () => {
-    const MediaPipeHands = handPoseDetection.SupportedModels.MediaPipeHands;
-    const detectorConfig = {
-      runtime: 'mediapipe',
-      solutionPath: 'node_modules/@mediapipe/hands',
-      maxHands: 2,
-      modelType: 'full',
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const handModel = await setHandDetector();
+        console.log('Model loaded');
+        setModel(handModel);
+      } catch (error) {
+        console.error('Error loading hand model:', error);
+      }
     };
-    const model = await handPoseDetection.createDetector(MediaPipeHands, detectorConfig);
-    console.log('model loaded', model);
-    setTimeout(() => {
-      setInterval(() => {
+    run();
+  }, []);
+
+  useEffect(() => {
+    if (model) {
+      const interval = setInterval(() => {
         detect(model);
       }, 10);
-    }, 5000);
-  }
-  run();
-  
-  async function detect(model) {
-    const video = webCamRef.current.video;
-    const { videoWidth } = video;
-    const { videoHeight } = video;
+      return () => clearInterval(interval);
+    }
+  }, [model]);
 
+  async function detect(model) {
+    if (typeof webCamRef.current === undefined ||
+        webCamRef.current === null ||
+        webCamRef.current.video.readyState !== 4
+      ) return;
+    const video = webCamRef.current.video;
+    // Get video dimensions
+    const { videoWidth, videoHeight } = video;
+
+    // Set video dimensions
+    video.width = videoWidth;
+    video.height = videoHeight;
+
+    // Set canvas dimensions
     canvasRef.current.width = videoWidth;
     canvasRef.current.height = videoHeight;
 
     const ctx = canvasRef.current.getContext('2d');
     const detections = await model.estimateHands(video);
-    console.log(detections, 'detections', '\n', model, 'model');
-
+    
+    // Clear the canvas before drawing
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+    
     draw(detections, ctx);
+    return;
   }
+
   return (
     <>
-      <Webcam 
-        ref={webCamRef}
-      />
-      <canvas 
-        ref={canvasRef}
-      />
+      <Webcam ref={webCamRef} />
+      <canvas ref={canvasRef} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
