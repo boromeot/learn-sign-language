8abcdbcf4aff0697011e6326456456e4cca61260
boromeot <boromeotyler@gmail.com>
2023-09-02 13:07:30 -0700

    added a modal and react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World</div>
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router}/>
+  // <App />
 )
