80870465b5cc489095d14d041a934839f68d3a31
boromeot <boromeotyler@gmail.com>
2023-09-02 14:42:27 -0700

    created link to lessons page from homepage

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,12 +1,13 @@
 import './home.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className='home-background'>
       <h1 className='home-heading'>Hand speak</h1>
-      <button className="home-cta">
+      <Link to={'/lessons'} className="home-cta btn">
         Let's go
-      </button>
+      </Link>
     </div>
   )
 }
