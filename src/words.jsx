558aad7322731a79620247770dec316517547060
boromeot <boromeotyler@gmail.com>
2023-09-05 10:19:38 -0700

    added more words

--- src/words.jsx
+++ src/words.jsx
@@ -1,18 +1,8 @@
 const lessons = {
-  '1' : ['Apple',
-        'Ant',
-        'Airplane',
-        'Astronaut',
-        'Ax',
-        'Anchor',
-        'Ambulance',
-        'Acorn',
-        'Ape',
-        'Aardvark',
-        'Alligator'],
-  '2' : ['Two'],
-  '3' : ['Three'],
-  '4' : ['Four']
+  '1' : ['Angle','Ant','Apple','Arch','Arm','Army','Baby','Bag','Ball','BanD','Basin','Basket','Bath','Bed','Bee','Bell','Berry','Bird','Blade','Board','Boat','Bone','Book','Boot','Bottle','Box','Boy','Brain','Brake','Branch','Brick','Bridge','Brush','Bucket','BulB','Button','Cake','Camera','Card','Cart','Carriage','Cat','Chain'],
+  '2' : ['Dog','Door','Drain','Drawer','Dress','Drop','Ear','Egg','Engine','Eye','Face','Farm','Feather','Finger','Fish','Flag','Floor','Fly','Foot','Fork','Fowl','Frame',],
+  '3' : ['Garden','Girl','Glove','Goat','Hair','Hammer','Hand','Hat','Head','Heart','Hook','Horn','Horse','Hospital','House','Island','Jewel'],
+  '4' : ['Kettle','Key','Knee','Knife','Knot','Leaf','Leg','Library','Line','Lip','Lock','Map','Match','Monkey','Moon','Mouth','Muscle','Nail','Neck','Needle']
 };
 
 export default lessons;
