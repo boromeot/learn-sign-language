7517918d1a40cdceb3fb4ad067263eac6924162c
boromeot <boromeotyler@gmail.com>
2023-09-05 10:04:55 -0700

    lesson buttons now use different words

--- src/routes/lessons/lessons.jsx
+++ src/routes/lessons/lessons.jsx
@@ -4,16 +4,16 @@ import './lessons.css';
 export default function Lessons() {
   return (
     <div className="lessons-background">
-      <Link to={'/app'} className='lesson-btn hlb1 btn'>
+      <Link to={'/lessons/1'} className='lesson-btn hlb1 btn'>
         Lesson 1
       </Link>
-      <Link to={'/app'} className='lesson-btn hlb2 btn'>
+      <Link to={'/lessons/2'} className='lesson-btn hlb2 btn'>
         Lesson 2
       </Link>
-      <Link to={'/app'} className='lesson-btn hlb3 btn'>
+      <Link to={'/lessons/3'} className='lesson-btn hlb3 btn'>
         Lesson 3
       </Link>
-      <Link to={'/app'} className='lesson-btn hlb4 btn'>
+      <Link to={'/lessons/4'} className='lesson-btn hlb4 btn'>
         Lesson 4
       </Link>
     </div>
---
+++ src/words.jsx
@@ -0,0 +1,18 @@
+const lessons = {
+  '1' : ['Apple',
+        'Ant',
+        'Airplane',
+        'Astronaut',
+        'Ax',
+        'Anchor',
+        'Ambulance',
+        'Acorn',
+        'Ape',
+        'Aardvark',
+        'Alligator'],
+  '2' : ['Two'],
+  '3' : ['Three'],
+  '4' : ['Four']
+};
+
+export default lessons;
