d9da4be57d97c439954c79594d683138514453ba
boromeot <boromeotyler@gmail.com>
2023-09-05 13:39:28 -0700

    added button effect to homepage

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div className='home-background'>
       <h1 className='home-heading'>Hand speak</h1>
-      <Link to={'/lessons'} className="home-cta btn">
+      <h3 className='home-heading-secondary'>An interactive sign language learning experience</h3>
+      <Link to={'/lessons'} className="home-cta btn btn1">
         Let's go
       </Link>
     </div>
