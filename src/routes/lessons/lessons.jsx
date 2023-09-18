be96ef159e8382ba9a63bb3a793eeaa8726ddc3f
boromeot <boromeotyler@gmail.com>
2023-09-18 10:48:39 -0700

    learning abc buttons now link to appropriate page with data

--- src/routes/lessons/lessons.jsx
+++ src/routes/lessons/lessons.jsx
@@ -5,10 +5,10 @@ export default function Lessons() {
   return (
     <div className="lessons-background">
       <div className='lessons-container'>
-        <h1 className='lessons-header'>Learning your abcs</h1>
-        <Link to={'/lessons/0'} className='btn btn1'>A - I</Link>
-        <Link to={'/lessons/0'} className='btn btn1'>J - R</Link>
-        <Link to={'/lessons/0'} className='btn btn1'>S - Z</Link>
+        <h1 className='lessons-header'>1.) Learning your abcs</h1>
+        <Link to={'/lessons/A-I'} className='btn btn1'>A - I</Link>
+        <Link to={'/lessons/J-R'} className='btn btn1'>J - R</Link>
+        <Link to={'/lessons/S-Z'} className='btn btn1'>S - Z</Link>
       </div>
       <Link to={'/lessons/1'} className='lesson-btn hlb1 btn btn1'>
         Lesson 1
